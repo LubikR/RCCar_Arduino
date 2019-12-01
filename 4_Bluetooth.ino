87109c1c0cb4d75e0f25d824d832dc652213ca6f
LubikR <renner.lubos@seznam.cz>
2019-12-01 21:02:17 +0100

    LEDky

--- 4_Bluetooth.ino
+++ 4_Bluetooth.ino
@@ -1,33 +1,56 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-int motor_1A = 2;
-int motor_1B = 3;
+/**
+ * S - Stop
+ * X - Set Speed
+ * R - Turn Right
+ * L - Turn Left
+ * B - Go Back
+ * F - Go Forward
+ * W - Front Leds
+ * H - Front Leds Long (high power)
+ * A - Left-turn Leds
+ * D - Right-turn Leds
+ **/
+
 int motor_2A = 4;
 int motor_2B = 7;
 int motor_1_PWM = 5;
 int motor_2_PWM = 6;
 
-//Rychlost blikani
-const long interval = 1000;
+#define pTrig 9
+#define pEcho 10
+#define LED_LEFT A0
+#define LED_RIGHT A1
+#define LED_FRONT 3
+#define LED_REVERSE_LIGHT A3
+#define LED_BACK 11
+#define motor_1A A5
+#define motor_1B A4
 
-int LED_RIGHT = 12;
-boolean LedRightState = false;
+#define LED_BACK_LOW_POWER 30
+
+//Rychlost blikani
+const long interval = 500;
+
+boolean LedRightRequest = false;
 unsigned long previousMillisRight = 0;
 int LED_RIGHT_STATE = LOW;
 
-int LED_LEFT = 12;
-boolean LedLeftState = false;
+boolean LedLeftRequest = false;
 unsigned long previousMillisLeft = 0;
 int LED_LEFT_STATE = LOW;
 
-int LED_FRONT = 12;
 boolean LedFrontState = false;
+boolean LedFrontHIGHStare = false;
 
-#define pTrig 9
-#define pEcho 10
+boolean stopState = false;
+unsigned long previousMillisFront = 0;
 
-SoftwareSerial bluetooth(11,12); //RX, TX
+boolean reverseState = false;
+
+SoftwareSerial bluetooth(12,A7); //RX, TX
 Servo servo;
 
 int servoPin = 8;
@@ -48,12 +71,10 @@ void setup() {
   pinMode(motor_1B, OUTPUT);
   pinMode(motor_2A, OUTPUT);
   pinMode(motor_2B, OUTPUT);
-  pinMode(motor_1_PWM, OUTPUT);
-  pinMode(motor_2_PWM, OUTPUT);
 
-  pinMode(LED_FRONT, OUTPUT);
-  pinMode(LED_LEFT, OUTPUT);
   pinMode(LED_RIGHT, OUTPUT);
+  pinMode(LED_LEFT, OUTPUT);
+  pinMode(LED_REVERSE_LIGHT, OUTPUT);
 
   pinMode(pTrig, OUTPUT);
   pinMode(pEcho, INPUT);
@@ -73,6 +94,10 @@ void setup() {
 
 void loop() {
   vzd_rovne = getVzdalenost();
+
+  //resetuji stopState a reserveState a nastavim si ho v kodu nize
+  stopState = false;
+  reverseState = false;
     
   bezpecna_vzdalenost = (speed / 10) + 25;
   int i = 0;
@@ -89,6 +114,9 @@ void loop() {
     Serial.print("Prijato : ");
     Serial.print(c[0]);
     Serial.println();
+
+    //reset FrontLimis abych ziskal vice jak 2sec od currentMillis()
+    previousMillisFront = 0;
 
     //tvrdy stop, nemam kam jet
     if (c[0] == 'F' && vzd_rovne < bezpecna_vzdalenost) { 
@@ -109,6 +137,7 @@ void loop() {
         break;
       case 'S':
         zastav();
+        stopState = true;
         break;
       case 'R':
         otoc_doprava();
@@ -118,31 +147,47 @@ void loop() {
         break;
       case 'B':
         jed_dozadu();
+        reverseState = true;
         break;
       case 'A':
-        if (LedLeftState) {
-          LedLeftState = false;
+        if (LedLeftRequest) {
+          LedLeftRequest = false;
         }
         else {
-          LedLeftState = true;
+          LedLeftRequest = true;
+          LedRightRequest = false;
         }
         break;
       case 'D':
-        if (LedRightState) {
-          LedRightState = false;
+        if (LedRightRequest) {
+          LedRightRequest = false;
         }
         else {
-          LedRightState = true;
+          LedRightRequest= true;
+          LedLeftRequest = false;
         }
         break;
       case 'W':
         if (LedFrontState) {
-          digitalWrite(LED_FRONT, LOW);
+          analogWrite(LED_FRONT, 0);
           LedFrontState = false;
         }
         else {
-          digitalWrite(LED_FRONT, HIGH);
+          analogWrite(LED_FRONT, 60);
+          LedFrontState = true;
         }
+        LedFrontHIGHStare = false;
+        break;
+      case 'H':
+        if (LedFrontHIGHStare) {
+          analogWrite(LED_FRONT, 0);
+          LedFrontHIGHStare = false;
+        }
+        else {
+          analogWrite(LED_FRONT, 255);
+          LedFrontHIGHStare = true;
+        }
+        LedFrontState = false;
         break;
      }
   }  
@@ -162,6 +207,7 @@ void loop() {
         break;
       case 'S':
         zastav();
+        stopState = true;
         break;
       case 'R':
         otoc_doprava();
@@ -171,12 +217,52 @@ void loop() {
         break;
       case 'B':
         jed_dozadu();
+        reverseState = true;
         break;
     }
   }
 
+  //Nastaveni vykonu brzdovych svetel
+  if (stopState) {
+    // pokud trva dlouho, tak jdu na nizsi vykon LED
+    unsigned long currentMillis = millis();
+    //set previousMillisFront if more than 5sec.
+    if (currentMillis - previousMillisFront > 5000) {
+      previousMillisFront = currentMillis;
+    }
+    //if more than 1sec go to low power, simulate that braking is gone
+    else if (currentMillis - previousMillisFront > 1500 && currentMillis - previousMillisFront < 5000)  
+    {
+      if (LedFrontState) {
+        analogWrite(LED_BACK, LED_BACK_LOW_POWER);
+      }
+      else {
+        analogWrite(LED_BACK, 0);
+      }
+      previousMillisFront = currentMillis - 1500;
+    }
+    //If less than 1 sec go full brake-power
+    else {
+      analogWrite(LED_BACK, 255);
+    }
+  }
+  else if (LedFrontState) {
+    analogWrite(LED_BACK, LED_BACK_LOW_POWER);
+  }
+  else {
+    analogWrite(LED_BACK, 0);
+  }
+
+  //Nastaveni svetel zpratecky
+  if (reverseState) {
+    digitalWrite(LED_REVERSE_LIGHT, HIGH);
+  }
+  else {
+    digitalWrite(LED_REVERSE_LIGHT, LOW);
+  }
+
   // Blikani levou
-  if (LedLeftState) {
+  if (LedLeftRequest) {
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillisLeft > interval) {
       previousMillisLeft = currentMillis;
@@ -187,11 +273,17 @@ void loop() {
         LED_LEFT_STATE = LOW;
       }
       digitalWrite(LED_LEFT, LED_LEFT_STATE);
+      // neblikal jsem pravou?
+      digitalWrite(LED_RIGHT, LOW);
     }
+  }
+  // pokud nechci blikat, tak tam navtrdo poslu LOW
+  else {
+    digitalWrite(LED_LEFT, LOW);
   }
 
   //Blikani pravou
-  if (LedRightState) {
+  if (LedRightRequest) {
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillisRight > interval) {
       previousMillisRight = currentMillis;
@@ -204,7 +296,10 @@ void loop() {
       digitalWrite(LED_RIGHT, LED_RIGHT_STATE);
     }
   }
-
+  // pokud nechci blikat, tak tam navtrdo poslu LOW
+  else {
+    digitalWrite(LED_RIGHT, LOW);
+  }
 }
 
 /*
